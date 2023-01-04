40b2304bc0e73c482f96d52d3b4158a9b9ec8ace
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-01-04 17:31:34 -0500

    adding lists to the CRUD program

---
+++ CRUD/README.md
@@ -0,0 +1,9 @@
+# CRUD
+
+>Creating a program to:
+Create
+Read
+Update
+Delete
+>With this program you will be able to add a client's name, search for a client's name, update a client's name and delete a client's name
+
--- CRUD/crud.py
+++ CRUD/crud.py
@@ -1,26 +1,26 @@
 import sys
-clients = 'kelly, leonardo,'
+clients = ["kelly", "leonardo"]
 
 def create_client(client_name):
 	global clients
 
 	if client_name not in clients :
-		clients += client_name
-		_add_comma()
+		clients.append(client_name)		
 	else:
-		print ('Client already is in the client\'s list')
+		print ('Client already in the client\'s list')
+
 
 def list_clients():
-	global clients
-
-	print(clients)
+	for idx, client in enumerate(clients):
+		print(f"{idx} : {client}")
 
 
 def updated_client(client_name, updated_client_name):
 	global clients
 
 	if client_name in clients :
-		clients = clients.replace (client_name + ',', updated_client_name + ',' )
+		index=clients.index(client_name)		
+		clients[index]=updated_client_name
 	else:
 		print ('Client is no it client\'s list')
 
@@ -29,33 +29,17 @@ def delete_client(client_name):
 	global clients
 
 	if client_name in clients:
-		clients = clients.replace(client_name + ',', '')
+		clients.remove(client_name)
+		
 	else:
 		print('Client is not in client\'s list')
 
 def search_client(client_name):
-	clients_list = clients.split(',')
-
-	for client in clients_list:
+	for client in clients:
 		if client != client_name:
 			continue
 		else:
 			return True
-
-def _add_comma():
-	global clients
-
-	clients += ','
-
-
-def _print_welcome():
-	print ('WELCOME')
-	print ('What would you like to do? ')
-	print ('[C]reate client')
-	print ('[L]ist client')
-	print ('[U]pdated client')
-	print ('[D]elete client')
-	print ('[S]earch client')
 
 
 def _get_client_name ():
@@ -72,6 +56,16 @@ def _get_client_name ():
 			sys.exit()
 
 	return client_name
+
+
+def _print_welcome():
+	print ('WELCOME')
+	print ('What would you like to do? ')
+	print ('[C]reate client')
+	print ('[L]ist client')
+	print ('[U]pdated client')
+	print ('[D]elete client')
+	print ('[S]earch client')
 
 if __name__ == '__main__':
 	_print_welcome()
