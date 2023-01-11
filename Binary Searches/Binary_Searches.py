097acd974e47cd79bed2c3e074ae38c05e4faa20
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-01-10 21:44:15 -0500

    adding Binary searches programs

---
+++ Binary Searches/Binary_Searches.py
@@ -0,0 +1,26 @@
+import random
+
+def binary_search(data, target, low, high):
+    if low > high:
+        return False
+    mid = (low + high) // 2
+    if target == data[mid]:
+        return True
+    elif target < data[mid]:
+        return binary_search(data,target,low,mid-1)
+    else:
+        return binary_search(data,target,mid+1,high)
+
+
+
+
+if __name__ == '__main__':
+
+    data = [random.randint(0, 100) for i in range(10)]
+
+    data.sort()
+
+    print(data)
+    target = int(input('What number would you like to find?'))
+    found = binary_search(data, target, 0, len(data)-1)
+    print(found)
